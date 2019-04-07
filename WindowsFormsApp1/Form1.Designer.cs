cb8ec340a64a3346b8369ae47cf9958a148c0e06
jinwook19 <drum0977@gmail.com>
2019-04-07 15:42:53 +0900

    cswinform

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textKorean = new System.Windows.Forms.TextBox();
+            this.textEnglish = new System.Windows.Forms.TextBox();
+            this.textMath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textAverage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "평균구하기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textKorean
+            // 
+            this.textKorean.Location = new System.Drawing.Point(56, 33);
+            this.textKorean.Name = "textKorean";
+            this.textKorean.Size = new System.Drawing.Size(243, 25);
+            this.textKorean.TabIndex = 1;
+            // 
+            // textEnglish
+            // 
+            this.textEnglish.Location = new System.Drawing.Point(56, 82);
+            this.textEnglish.Name = "textEnglish";
+            this.textEnglish.Size = new System.Drawing.Size(243, 25);
+            this.textEnglish.TabIndex = 2;
+            // 
+            // textMath
+            // 
+            this.textMath.Location = new System.Drawing.Point(56, 130);
+            this.textMath.Name = "textMath";
+            this.textMath.Size = new System.Drawing.Size(243, 25);
+            this.textMath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "국어";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "영어";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "수학";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-4, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "평균";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // textAverage
+            // 
+            this.textAverage.Enabled = false;
+            this.textAverage.Location = new System.Drawing.Point(56, 177);
+            this.textAverage.Name = "textAverage";
+            this.textAverage.Size = new System.Drawing.Size(243, 25);
+            this.textAverage.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textMath);
+            this.Controls.Add(this.textEnglish);
+            this.Controls.Add(this.textKorean);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textKorean;
+        private System.Windows.Forms.TextBox textEnglish;
+        private System.Windows.Forms.TextBox textMath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textAverage;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Int32 result = Convert.ToInt32(textKorean.Text) + Convert.ToInt32(textEnglish.Text) + Convert.ToInt32(textMath.Text);
+                MessageBox.Show(Convert.ToString(result));
+                textAverage.Text = Convert.ToString(result / 3);
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message == "입력 문자열의 형식이 잘못되었습니다.")
+                {
+                    MessageBox.Show("숫자값을 입력하세요");
+
+                }
+                else
+                {
+                    MessageBox.Show(exception.Message);
+
+                }
+
+            }
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
