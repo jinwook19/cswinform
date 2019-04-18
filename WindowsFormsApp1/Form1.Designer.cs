5a77b9be67ee411fbbf0996342124dee52263f7b
drum0977 <drum0977@gmail.com>
2019-04-19 00:46:28 +0900

    연습

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -6,7 +6,6 @@
         /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-        //아이고 힘들어라
 
         /// <summary>
         /// 사용 중인 모든 리소스를 정리합니다.
